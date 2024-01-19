2b3353ee4a14227c00ca4740c3e1b355fb6e8421
therealdantec <dciarabellini@yahoo.com>
2024-01-19 08:40:19 -0700

    added json functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -20,7 +21,10 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        # TODO: Add code to check the username and password, and log the user in
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        if username in users and users[username] == password:
+            return redirect(url_for('create_quiz'))
     return render_template('login.html')
 
 @app.route('/create_account', methods=['GET', 'POST'])
@@ -28,9 +32,18 @@ def create_account():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        # TODO: Add code to create the account with the provided username and password
-        # For example, you might store the account information in a database
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        users[username] = password
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+        return redirect(url_for('create_quiz'))
     return render_template('create_account.html')
+
+@app.route('/create_quiz', methods=['GET', 'POST'])
+def create_quiz():
+    # TODO: Add code to handle quiz creation
+    return render_template('create_quiz.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
